a9cbbe6b93c7ec1a09ff81412a814bb17d4e104d
Cheng Guan <chengguan.teo@gmail.com>
2021-09-22 20:08:22 +0800

    set level for module logger

--- app.py
+++ app.py
@@ -2,20 +2,26 @@
 import logging
 from my_module import *
 
-def main():
-    logging.basicConfig(filename='myapp.log', level=logging.INFO, 
-                format='%(asctime)s [%(module)s:%(funcName)s] %(message)s', 
-                datefmt='%m/%d/%Y %H:%M:%S')
+module_logger = logging.getLogger('my_module').setLevel(level=logging.WARNING)
 
-    # import logging.config
-    # logging.config.fileConfig('logging.conf')
-    # your program code
+def main():
+    logging.basicConfig(filename='myapp.log', 
+                level=logging.INFO, 
+                format='%(asctime)s [%(module)s:%(funcName)s] %(message)s')
+    logger = logging.getLogger(__name__)
+
+    fh = logging.FileHandler('root.log')
+    fh.setLevel(logging.WARNING)
+
+    root_logger = logging.getLogger()
+    root_logger.addHandler(fh)
 
     foo()
 
-    b=Bar()
+    b = Bar()
     b.bar()
-    logging.info('test.')
+    logger.info('test.')
+    root_logger.info('root')
 
 if __name__ == '__main__':
     main()
--- my_module.py
+++ my_module.py
@@ -1,9 +1,14 @@
 #my_module
 import logging
 
+logger = logging.getLogger(__name__)
+
 def foo():
-    logging.info('Hi, foo')
+    logger.info('Hi, foo, info.')
+    logger.warning('Hi, foo, warning.')
 
 class Bar(object):
     def bar(self):
-        logging.info('Hi, bar')
+        root_logger = logging.getLogger()
+        root_logger.error('error')
+        logger.info('Hi, bar')
